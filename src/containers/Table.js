cac68946f620b43453c5438ae5574041d8804c66
macbook <kir-esakov2011@mail.ru>
2019-04-27 18:01:21 +0300

    experimentations; nothing has changed

--- src/containers/Table.js
+++ src/containers/Table.js
@@ -5,6 +5,15 @@ import * as actionCreators from "../actions"
 import LoadingSpinner from "../components/LoadingSpinner";
 
 class Table extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSorting = this.handleSorting.bind(this);
+  }
+
+  handleSorting(event) {
+
+  }
+
   render() {
     const {shows} = this.props;
 
@@ -15,29 +24,37 @@ class Table extends React.Component {
     //   return "No such shows ;("
     // }
 
-    const showsElems = shows.info && shows.info.map(show =>
-      <tr key={show.id}>
-        <td>{show.id}</td>
-        <td>
-          <a href={"https://trakt.tv/shows/" + show.slug} target="_blank" rel="noopener noreferrer">
-            {show.title}
-          </a>
-        </td>
-        <td>{show.overview ? show.overview : "¯\\_(ツ)_/¯"}</td>
-        <td>{show.year ? show.year : "¯\\_(ツ)_/¯"}</td>
-        <td>{show.rating ? show.rating : "¯\\_(ツ)_/¯"}</td>
-        <td>
-          {!show.posterUrl ? <LoadingSpinner/> :
-            show.posterUrl === "no posters" ? <p>Only logo :(</p> :
-              show.posterUrl === "no info" ? <p>No posters ;(</p> :
-                <a href={show.posterUrl} rel="noopener noreferrer" target="_blank">
-                  {!show.imgLoaded ? <LoadingSpinner/> : ""}
-                  <img src={show.posterUrl} className="poster" alt={show.title + " poster"}
-                       onLoad={() => this.props.revealImg(show.id)} hidden={!show.imgLoaded}/>
-                </a>
-          }
-        </td>
-      </tr>
+    const showsElems = shows.info && shows.info.map(show => {
+        const overview = show.overview ? show.overview : "¯\\_(ツ)_/¯";
+        const year = show.year ? show.year : "¯\\_(ツ)_/¯";
+        // const rating = !show.rating || !show.votes ? "¯\\_(ツ)_/¯" : <span>
+        //   <p>{show.rating}</p>*<p>{show.votes}</p>={show.rating * show.votes}
+        // </span>;
+        const rating = show.rating;
+
+        return <tr key={show.id}>
+          <td>{show.id}</td>
+          <td>
+            <a href={"https://trakt.tv/shows/" + show.slug} target="_blank" rel="noopener noreferrer">
+              {show.title}
+            </a>
+          </td>
+          <td>{overview}</td>
+          <td>{year}</td>
+          <td>{rating}</td>
+          <td>
+            {!show.posterUrl ? <LoadingSpinner/> :
+              show.posterUrl === "no posters" ? <p>Only logo :(</p> :
+                show.posterUrl === "no info" ? <p>No posters ;(</p> :
+                  <a href={show.posterUrl} rel="noopener noreferrer" target="_blank">
+                    {!show.imgLoaded ? <LoadingSpinner/> : ""}
+                    <img src={show.posterUrl} className="poster" alt={show.title + " poster"}
+                         onLoad={() => this.props.revealImg(show.id)} hidden={!show.imgLoaded}/>
+                  </a>
+            }
+          </td>
+        </tr>
+      }
     );
 
     return (
@@ -45,12 +62,13 @@ class Table extends React.Component {
         <table>
           <thead>
             <tr>
-              <td>№</td>
-              <td>Title</td>
-              <td>Overview</td>
-              <td>Year</td>
-              <td>Rating</td>
-              <td>Poster</td>
+              <th className={"No"} onClick={() => this.props.setSorting("No")}>№</th>
+              <th>Title</th>
+              <th>Overview</th>
+              <th>Year</th>
+              <th>Rating</th>
+              {/*<th className={"ascend"}>Rating * Votes number</th>*/}
+              <th>Poster</th>
             </tr>
           </thead>
           <tbody>{showsElems}</tbody>
