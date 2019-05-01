e950e4433879018f374f4ceb06285b741635ad48
macbook <kir-esakov2011@mail.ru>
2019-05-01 15:28:05 +0300

    anonymous funcs in actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -30,195 +30,157 @@ const traktOptions = {
 
 // Getting all shows info
 
-export function fetchShows(pageNumber, itemsPerPage) {
-  return dispatch => {
-    dispatch(requestShows());
+export const fetchShows = (pageNumber, itemsPerPage) => dispatch => {
+  dispatch(requestShows());
 
-    const url = "https://api.trakt.tv/shows/popular?extended=full&page=" + pageNumber + "&limit=" + itemsPerPage;
-    return fetch(url, traktOptions)
-      .then(response => {
-        if (!response.ok) {
-          dispatch(failSearchShows())
-        } else {
-          dispatch(receivePaginationInfo(response));
-          return response.json()
-        }
-      })
-      .then(json => {
-        dispatch(receiveShows(json));
-        json.map(info => dispatch(fetchPosterInfo(info.ids.tvdb)));
-      });
-  }
-}
+  const url = "https://api.trakt.tv/shows/popular?extended=full&page=" + pageNumber + "&limit=" + itemsPerPage;
+  return fetch(url, traktOptions)
+    .then(response => {
+      if (!response.ok) {
+        dispatch(failSearchShows())
+      } else {
+        dispatch(receivePaginationInfo(response));
+        return response.json()
+      }
+    })
+    .then(json => {
+      dispatch(receiveShows(json));
+      json.map(info => dispatch(fetchPosterInfo(info.ids.tvdb)));
+    });
+};
 
-function requestShows() {
-  return {
-    type: REQUEST_SHOWS
-  }
-}
+const requestShows = () => ({
+  type: REQUEST_SHOWS
+});
 
-function receiveShows(shows) {
-  return {
-    type: RECEIVE_SHOWS,
-    shows: shows,
-  }
-}
+const receiveShows = shows => ({
+  type: RECEIVE_SHOWS,
+  shows: shows,
+});
 
-export function receivePaginationInfo(response) {
-  return {
-    type: RECEIVE_PAGINATION_INFO,
-    pageCount: parseInt(response.headers.get("X-Pagination-Page-Count")),
-    itemCount: parseInt(response.headers.get("X-Pagination-Item-Count")),
-  }
-}
+export const receivePaginationInfo = response => ({
+  type: RECEIVE_PAGINATION_INFO,
+  pageCount: parseInt(response.headers.get("X-Pagination-Page-Count")),
+  itemCount: parseInt(response.headers.get("X-Pagination-Item-Count")),
+});
 
 // Search
 
-export function setSearchValue(value) {
-  return {
-    type: SET_SEARCH_VALUE,
-    value: value
-  }
-}
+export const setSearchValue = value => ({
+  type: SET_SEARCH_VALUE,
+  value: value
+});
 
-export function searchShows(value, searchMode, pageNumber, itemsPerPage) {
-  return dispatch => {
-    dispatch(requestSearchShows());
-    console.log("value="+ value);
-    let searchByField;
-    switch (searchMode) {
-      case "title":
-        searchByField = "title";
-        break;
-      case "overview":
-        searchByField = "overview";
-        break;
-      case "anyLangTitle":
-        searchByField = "aliases";
-        break;
-      default:
-        searchByField = "";
-    }
-    const url = "https://api.trakt.tv/search/show?extended=full&field=" + searchByField + "&query=" + value +
-      "&page=" + pageNumber + "&limit=" + itemsPerPage;
-    return fetch(url, traktOptions)
-      .then(response => {
-        if (!response.ok) {
-          dispatch(failSearchShows())
-        } else {
-          dispatch(receivePaginationInfo(response));
-          return response.json()
-        }
-      })
-      .then(json => {
-        dispatch(receiveSearchShows(json));
-        json.map(info => dispatch(fetchPosterInfo(info.show.ids.tvdb)));
-      });
+export const searchShows = (value, searchMode, pageNumber, itemsPerPage) => dispatch => {
+  dispatch(requestSearchShows());
+  console.log("value=" + value);
+  let searchByField;
+  switch (searchMode) {
+    case "title":
+      searchByField = "title";
+      break;
+    case "overview":
+      searchByField = "overview";
+      break;
+    case "anyLangTitle":
+      searchByField = "aliases";
+      break;
+    default:
+      searchByField = "";
   }
-}
+  const url = "https://api.trakt.tv/search/show?extended=full&field=" + searchByField + "&query=" + value +
+    "&page=" + pageNumber + "&limit=" + itemsPerPage;
+  return fetch(url, traktOptions)
+    .then(response => {
+      if (!response.ok) {
+        dispatch(failSearchShows())
+      } else {
+        dispatch(receivePaginationInfo(response));
+        return response.json()
+      }
+    })
+    .then(json => {
+      dispatch(receiveSearchShows(json));
+      json.map(info => dispatch(fetchPosterInfo(info.show.ids.tvdb)));
+    });
+};
 
-function requestSearchShows() {
-  return {
-    type: REQUEST_SEARCH_SHOWS
-  }
-}
+const requestSearchShows = () => ({
+  type: REQUEST_SEARCH_SHOWS
+});
 
-function receiveSearchShows(shows) {
-  return {
-    type: RECEIVE_SEARCH_SHOWS,
-    shows: shows,
-  }
-}
+const receiveSearchShows = shows => ({
+  type: RECEIVE_SEARCH_SHOWS,
+  shows: shows,
+});
 
-function failSearchShows() {
-  return {
-    type: FAIL_SEARCH_SHOWS
-  }
-}
+const failSearchShows = () => ({
+  type: FAIL_SEARCH_SHOWS
+});
 
 
 // Getting poster info
 
-export function fetchPosterInfo(posterId) {
-  return dispatch => {
-    dispatch(requestPosterInfo(posterId));
+export const fetchPosterInfo = posterId => dispatch => {
+  dispatch(requestPosterInfo(posterId));
 
-    const url = "https://webservice.fanart.tv/v3/tv/" + posterId + "?api_key=" + FANART_PROJECT_KEY +
-      "&client_key=" + FANART_PERSONAL_KEY;
-    return fetch(url)
-      .then(response => {
-        if (!response.ok) {
-          dispatch(failPosterInfo(posterId))
-        } else {
-          return response.json()
-        }
-      })
-      .then(json => dispatch(receivePosterInfo(json, posterId)));
-  }
-}
+  const url = "https://webservice.fanart.tv/v3/tv/" + posterId + "?api_key=" + FANART_PROJECT_KEY +
+    "&client_key=" + FANART_PERSONAL_KEY;
+  return fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        dispatch(failPosterInfo(posterId))
+      } else {
+        return response.json()
+      }
+    })
+    .then(json => dispatch(receivePosterInfo(json, posterId)));
+};
 
-export function requestPosterInfo(posterId) {
-  return {
-    type: REQUEST_POSTER_INFO,
-    posterId: posterId
-  }
-}
+export const requestPosterInfo = posterId => ({
+  type: REQUEST_POSTER_INFO,
+  posterId: posterId
+});
 
-export function receivePosterInfo(info, posterId) {
-  return {
-    type: RECEIVE_POSTER_INFO,
-    info: info,
-    posterId: posterId
-  }
-}
+export const receivePosterInfo = (info, posterId) => ({
+  type: RECEIVE_POSTER_INFO,
+  info: info,
+  posterId: posterId
+});
 
-export function failPosterInfo(posterId) {
-  return {
-    type: FAIL_POSTER_INFO,
-    posterId: posterId
-  }
-}
+export const failPosterInfo = posterId => ({
+  type: FAIL_POSTER_INFO,
+  posterId: posterId
+});
 
-export function revealImg(showId) {
-  return {
-    type: SHOW_IMG,
-    showId: showId
-  }
-}
+export const revealImg = showId => ({
+  type: SHOW_IMG,
+  showId: showId
+});
 
 
 // Pagination
 
-export function setPage(pageNumber) {
-  return {
-    type: SET_PAGE,
-    pageNumber: pageNumber,
-  }
-}
+export const setPage = pageNumber => ({
+  type: SET_PAGE,
+  pageNumber: pageNumber,
+});
 
-export function changeItemsPerPage(value) {
-  return {
-    type: CHANGE_ITEMS_PER_PAGE,
-    items: value
-  }
-}
+export const changeItemsPerPage = value => ({
+  type: CHANGE_ITEMS_PER_PAGE,
+  items: value
+});
 
 
 // Mode
 
-export function setSearchMode(mode) {
-  return {
-    type: SET_SEARCH_MODE,
-    mode: mode,
-  }
-}
+export const setSearchMode = mode => ({
+  type: SET_SEARCH_MODE,
+  mode: mode,
+});
 
 
-
-
-export function showFullOverview(id) {
-  return {
-    type: SHOW_FULL_OVERVIEW,
-    id: id,
-  }
-}
+export const showFullOverview = id => ({
+  type: SHOW_FULL_OVERVIEW,
+  id: id,
+});
